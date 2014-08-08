6d7ee864aefe867ba2250ceeeafbc60e5bec8e0c
skipallmighty <skipallmighty@gmail.com>
2014-08-08 12:45:35 -0400

    Update AppDelegate.swift

--- TicTacToe/AppDelegate.swift
+++ TicTacToe/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  TicTacToe
 //
-//  Created by Jacob Schatz on 6/5/14.
+//  Created by Skip Wilson on 6/5/14.
 //  Copyright (c) 2014 Bisonkick. All rights reserved.
 //
 
@@ -62,7 +62,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Returns the managed object context for the application.
     // If the context doesn't already exist, it is created and bound to the persistent store coordinator for the application.
     var managedObjectContext: NSManagedObjectContext {
-        if !_managedObjectContext {
+        if _managedObjectContext !== nil {
             let coordinator = self.persistentStoreCoordinator
             if coordinator != nil {
                 _managedObjectContext = NSManagedObjectContext()
@@ -76,7 +76,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Returns the managed object model for the application.
     // If the model doesn't already exist, it is created from the application's model.
     var managedObjectModel: NSManagedObjectModel {
-        if !_managedObjectModel {
+        if _managedObjectModel !== nil {
             let modelURL = NSBundle.mainBundle().URLForResource("TicTacToe", withExtension: "momd")
             _managedObjectModel = NSManagedObjectModel(contentsOfURL: modelURL)
         }
@@ -87,7 +87,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Returns the persistent store coordinator for the application.
     // If the coordinator doesn't already exist, it is created and the application's store added to it.
     var persistentStoreCoordinator: NSPersistentStoreCoordinator {
-        if !_persistentStoreCoordinator {
+        if _persistentStoreCoordinator !== nil {
             let storeURL = self.applicationDocumentsDirectory.URLByAppendingPathComponent("TicTacToe.sqlite")
             var error: NSError? = nil
             _persistentStoreCoordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
